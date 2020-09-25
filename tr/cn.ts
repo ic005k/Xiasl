81f4de8b897d5d83aa207687d4346a83fd86935f
ic005k <zh99889@126.com>
2020-09-25 14:28:36 +0800

    Add files via upload

---
+++ tr/cn.ts
@@ -0,0 +1,319 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <source>CaseSensitive</source>
+        <translation type="unfinished">区分大小写</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <source>FindPrevious</source>
+        <translation type="unfinished">查找上一个</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>FindNext</source>
+        <translation type="unfinished">查找下一个</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="55"/>
+        <source>Replace</source>
+        <translation type="unfinished">替换</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="62"/>
+        <source>Replace &amp;&amp; Find</source>
+        <translation type="unfinished">替换并查找</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>&lt;&lt;&lt;&lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <source>&gt;&gt;&gt;&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <source>RefreshTree</source>
+        <translation type="unfinished">刷新树图</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="151"/>
+        <source>M</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="167"/>
+        <source>D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="183"/>
+        <source>S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="206"/>
+        <source>BasicInfo</source>
+        <translation type="unfinished">基本信息</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="228"/>
+        <location filename="mainwindow.cpp" line="3565"/>
+        <source>Errors</source>
+        <translation type="unfinished">错误</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <location filename="mainwindow.cpp" line="3567"/>
+        <source>Warnings</source>
+        <translation type="unfinished">警告</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="272"/>
+        <location filename="mainwindow.cpp" line="3569"/>
+        <source>Remarks</source>
+        <translation type="unfinished">提醒</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="294"/>
+        <source>Optimizations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="327"/>
+        <source>File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="339"/>
+        <source>Tools</source>
+        <translation type="unfinished">工具</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="349"/>
+        <source>Preferences</source>
+        <translation type="unfinished">选项</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="361"/>
+        <source>Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="366"/>
+        <source>Save</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="371"/>
+        <source>SaveAS...</source>
+        <translation type="unfinished">另存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="376"/>
+        <source>Generate</source>
+        <translation type="unfinished">获取</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="381"/>
+        <source>Compiling</source>
+        <translation type="unfinished">编译</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="386"/>
+        <location filename="mainwindow.cpp" line="153"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="391"/>
+        <source>New</source>
+        <translation type="unfinished">新建</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="396"/>
+        <source>字体-Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="407"/>
+        <source>Font</source>
+        <translation type="unfinished">字体</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="415"/>
+        <source>WrapWord</source>
+        <translation type="unfinished">自动换行</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="420"/>
+        <source>Kextstat</source>
+        <translation type="unfinished">Kext加载状态</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="151"/>
+        <source>Last modified: </source>
+        <translation type="unfinished">最后修改：</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="214"/>
+        <location filename="mainwindow.cpp" line="309"/>
+        <location filename="mainwindow.cpp" line="367"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="215"/>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="243"/>
+        <source>File loaded</source>
+        <translation type="unfinished">文件已装入</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="310"/>
+        <source>The document has been modified.
+Do you want to save your changes?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="357"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="361"/>
+        <source>Cannot open file %1 for writing:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="372"/>
+        <source>File saved</source>
+        <translation type="unfinished">文件已保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="466"/>
+        <source>Empty search field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="466"/>
+        <source>The search field is empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="563"/>
+        <source>Compiled</source>
+        <translation>编译完成</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="565"/>
+        <source>编译完成</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3577"/>
+        <source>错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3579"/>
+        <source>警告</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3581"/>
+        <source>提醒</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Compilation successful.</source>
+        <translation type="vanished">编译成功</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1378"/>
+        <source>Refresh completed</source>
+        <translation>刷新完成</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1380"/>
+        <source>刷新完成</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1448"/>
+        <source>Refreshing...</source>
+        <translation>刷新中...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3040"/>
+        <source>ctrl+n</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3043"/>
+        <source>ctrl+o</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3046"/>
+        <source>ctrl+s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3049"/>
+        <source>ctrl+shift+s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3054"/>
+        <source>ctrl+g</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3057"/>
+        <source>ctrl+m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3060"/>
+        <source>ctrl+f</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="3063"/>
+        <source>ctrl+w</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecentFiles</name>
+    <message>
+        <location filename="recentfiles.cpp" line="26"/>
+        <source>Open Recent...</source>
+        <translation>最近打开...</translation>
+    </message>
+</context>
+</TS>
